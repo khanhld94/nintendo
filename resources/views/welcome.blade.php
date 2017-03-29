8aa5ad2edef40109b09e206650b5bdfaae0a345d
Lê Duy Khánh <leduykhanh.hedspi@gmail.com>
2017-03-29 15:03:57 +0000

    fix home page view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,9 +12,19 @@
         <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet" type="text/css">
 
         <!-- Loading main css file -->
+        <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/bootstrap-theme.css') }}">
         <link rel="stylesheet" href="{{ asset('css/client-side-style.css') }}">
         <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
-        
+        <style type="text/css">
+            @font-face {
+                font-family: fipps;
+                src: url('{{ asset('/fonts/Fipps-Regular.otf') }}');
+            }
+            .example3 {
+                font-family: fipps;
+            }
+        </style>
         <!--[if lt IE 9]>
         <script src="js/ie-support/html5.js"></script>
         <script src="js/ie-support/respond.js"></script>
@@ -22,157 +32,50 @@
 
     </head>
     <body>
-        
-
+        <div class="example3">
+          <nav class="navbar navbar-inverse navbar-static-top">
+            <div class="container">
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar3">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="{{ route('home') }}"><img src="{{ asset('/images/gamepad.png')}}" alt="Nintendo World">
+                </a>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">Nintendo World</a></li>
+                </ul>
+              </div>
+              <div id="navbar3" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav navbar-right">
+                  <li class="active"><a href="#">Home</a></li>
+                  <li><a href="#">About</a></li>
+                  <li><a href="#">Contact</a></li>
+                  <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                    <ul class="dropdown-menu" role="menu">
+                      <li><a href="#">Action</a></li>
+                      <li><a href="#">Another action</a></li>
+                      <li><a href="#">Something else here</a></li>
+                      <li class="divider"></li>
+                      <li class="dropdown-header">Nav header</li>
+                      <li><a href="#">Separated link</a></li>
+                      <li><a href="#">One more separated link</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+              <!--/.nav-collapse -->
+            </div>
+            <!--/.container-fluid -->
+          </nav>
+        </div>
         <div id="site-content">
-            <header class="site-header">
-                <div class="container">
-                    <a href="{{ route('home') }}" id="branding">
-                        <img src="images/logo.png" alt="" class="logo">
-                        <div class="logo-copy">
-                            <h1 class="site-title">Company Name</h1>
-                            <small class="site-description">Tagline goes here</small>
-                        </div>
-                    </a> <!-- #branding -->
+            
+            @yield('content')
 
-                    <div class="main-navigation">
-                        <button type="button" class="menu-toggle"><i class="fa fa-bars"></i></button>
-                        <ul class="menu">
-                            <li class="menu-item current-menu-item"><a href="index.html">Home</a></li>
-                            <li class="menu-item"><a href="about.html">About</a></li>
-                            <li class="menu-item"><a href="review.html">Movie reviews</a></li>
-                            <li class="menu-item"><a href="joinus.html">Join us</a></li>
-                            <li class="menu-item"><a href="contact.html">Contact</a></li>
-                        </ul> <!-- .menu -->
-
-                        <form action="#" class="search-form">
-                            <input type="text" placeholder="Search...">
-                            <button><i class="fa fa-search"></i></button>
-                        </form>
-                    </div> <!-- .main-navigation -->
-
-                    <div class="mobile-navigation"></div>
-                </div>
-            </header>
-            <main class="main-content">
-                <div class="container">
-                    <div class="page">
-                        <div class="row">
-                            <div class="col-md-9">
-                                <div class="slider">
-                                    <ul class="slides">
-                                        <li><a href="#"><img src="{{ asset('dummy/slide-1.png') }}" alt="Slide 1"></a></li>
-                                        <li><a href="#"><img src="{{ asset('dummy/slide-2.jpg') }}" alt="Slide 2"></a></li>
-                                        <li><a href="#"><img src="{{ asset('dummy/slide-3.jpg') }}" alt="Slide 3"></a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div class="col-md-3">
-                                <div class="row">
-                                    <h2 style="text-align: center">Top games</h2>
-                                    <table class="table table-striped table-bordered table-hover" id="dataTables-example">
-                                        <tbody>
-                                            @foreach ($top_games as $game)
-                                                <tr class="col-md-12" id="top_game">
-                                                    <td class="col-md-4" id="top_game_image">
-                                                        <img src="/resource/upload/game_image/{{ $game->image }}"></td>
-                                                    <td class="col-md-8" id="top_game_title">
-                                                       <div id="game_name">
-                                                         <a href="{{ route('games.show', $game->id )}}">{{ $game->name }}</a>
-                                                       </div>
-                                                       <div>
-                                                         System: {{ $game->system->name }}
-                                                       </div>
-                                                    </td>
-                                                </tr>
-                                            @endforeach
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                        </div> <!-- .row -->
-                        <div class="row">
-                            <div class="movie-list">
-                                @foreach ($games as $game)
-                                    <div class="movie">
-                                        <figure class="movie-poster"><img src="/resource/upload/game_image/{{ $game->image }}" alt="#"></figure>
-                                        <div class="movie-title"><a href="{{ route('games.show', $game->id )}}">{{ $game->name }}</a></div>
-                                    </div>
-                                @endforeach
-                            </div> <!-- .movie-list -->
-                        </div> <!-- .row -->
-                        
-                        <div class="row">
-                            <div class="col-md-4">
-                                <h2 class="section-title">December premiere</h2>
-                                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                                <ul class="movie-schedule">
-                                    <li>
-                                        <div class="date">16/12</div>
-                                        <h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-                                    </li>
-                                    <li>
-                                        <div class="date">16/12</div>
-                                        <h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-                                    </li>
-                                    <li>
-                                        <div class="date">16/12</div>
-                                        <h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-                                    </li>
-                                    <li>
-                                        <div class="date">16/12</div>
-                                        <h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-                                    </li>
-                                </ul> <!-- .movie-schedule -->
-                            </div>
-                            <div class="col-md-4">
-                                <h2 class="section-title">November premiere</h2>
-                                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                                <ul class="movie-schedule">
-                                    <li>
-                                        <div class="date">16/12</div>
-                                        <h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-                                    </li>
-                                    <li>
-                                        <div class="date">16/12</div>
-                                        <h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-                                    </li>
-                                    <li>
-                                        <div class="date">16/12</div>
-                                        <h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-                                    </li>
-                                    <li>
-                                        <div class="date">16/12</div>
-                                        <h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-                                    </li>
-                                </ul> <!-- .movie-schedule -->
-                            </div>
-                            <div class="col-md-4">
-                                <h2 class="section-title">October premiere</h2>
-                                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                                <ul class="movie-schedule">
-                                    <li>
-                                        <div class="date">16/12</div>
-                                        <h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-                                    </li>
-                                    <li>
-                                        <div class="date">16/12</div>
-                                        <h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-                                    </li>
-                                    <li>
-                                        <div class="date">16/12</div>
-                                        <h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-                                    </li>
-                                    <li>
-                                        <div class="date">16/12</div>
-                                        <h2 class="entry-title"><a href="#">Perspiciatis unde omnis</a></h2>
-                                    </li>
-                                </ul> <!-- .movie-schedule -->
-                            </div>
-                        </div>
-                    </div>
-                </div> <!-- .container -->
-            </main>
             <footer class="site-footer">
                 <div class="container">
                     <div class="row">
@@ -242,13 +145,10 @@
             </footer>
         </div>
         <!-- Default snippet for navigation -->
-        
-
-
         <script src="{{ asset('js/jquery-1.8.3.min.js') }}"></script>
+        <script src="{{ asset('js/bootstrap.min.js') }}"></script>
         <script src="{{ asset('js/plugins.js') }}"></script>
-        <script src="{{ asset('js/custom.js') }}"></script>
-        
+        <script src="{{ asset('js/custom.js') }}"></script>   
     </body>
 
 </html>
